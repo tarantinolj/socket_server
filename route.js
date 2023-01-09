5699ec059111290a129e448992fd621065d53a63
AndrewSluinkou <tarantinolj@mail.ru>
2023-01-09 19:51:08 +0300

    sever connectt

--- route.js
+++ route.js
@@ -1,20 +1,17 @@
-const express = require("express");
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
-router.get("/", (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
+router.get('/', (req, res) => {
+  res.setHeader({ 'Access-Control-Allow-Origin': '*' })
 
   res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+  )
 
-  res.send("Это только мой мир.");
-});
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
 
-module.exports = router;
+  res.send('Это только мой мир.')
+})
+
+module.exports = router
